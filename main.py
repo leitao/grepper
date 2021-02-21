b5e1913185d7144d05b16183d0cdf26cfcc46b4b
Breno Leitao <leitao@debian.org>
2021-02-21 16:45:58 +0000

    Working with buffers

--- main.py
+++ main.py
@@ -4,17 +4,41 @@ import threading
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
 import time
+import sys
 
 cur_pos = 2
 wsize = 15
 hsize = 2
 titles = []
 curtab = -1
-# pristine input
-pristine = []
+quitting = False
+EOF = False
 
 # refresh timeout
 TIMEOUT = 100
+
+class Buffer:
+    # Main buffer
+    main = []
+    # Buffer when the ingestion is paused
+    tmp = []
+
+    paused = False
+
+    def pause(self):
+        self.paused = True
+
+    def unpause(self):
+        self.paused = False
+        self.main = self.main + self.tmp
+        self.tmp = []
+
+    def ingest(self, line):
+        if not self.paused:
+            self.main.append(line)
+        else:
+            self.tmp.append(line)
+
 
 class Tab:
     name = ""
@@ -34,15 +58,22 @@ class Tab:
 
         # debug()
 
+
+# Ingestion Buffer
+pristine = Buffer()
+
+
 def delete_tab():
     global curtab
     titles.pop(curtab)
     curtab -= 1
 
+
 def add_new_tab():
     global curtab
     curtab += 1
     titles.append(Tab(f"Panel {curtab}"))
+
 
 def print_all_tabs(stdscr):
     global cur_pos
@@ -52,6 +83,7 @@ def print_all_tabs(stdscr):
     for idx, tab in enumerate(titles):
         print_tab(stdscr, tab, idx == curtab)
     stdscr.refresh()
+
 
 def print_tab(stdscr, tab, active):
     global cur_pos
@@ -109,7 +141,7 @@ def debug():
     pdb.set_trace()
 
 
-def print_text_box():
+def print_text_box(scroll):
     global curtab
     global pristine
     global titles
@@ -117,21 +149,19 @@ def print_text_box():
     win = curses.newwin(curses.LINES - 3, curses.COLS, 3, 0)
     win.scrollok(True)
 
-
-    for line in pristine:
-        # debug()
-        # win.addstr(line)
+    for line in pristine.main:
         if (curtab < 0) or (titles[curtab].grep in line):
             win.addstr(line)
-        # else:
-        #     win.addstr(f"Not {titles[curtab].grep} : " + line)
+
 
     win.refresh()
 
 
 def ingest_text(s):
     global pristine
-    pristine.append(s)
+
+    pristine.ingest(s)
+
 
 # Set find word
 def set_find(stdscr):
@@ -158,22 +188,26 @@ def set_find(stdscr):
     titles[curtab].grep = word.strip()
 
 
-def draw_screen(stdscr):
+def draw_screen(stdscr, scroll):
     stdscr.clear()
     print_all_tabs(stdscr)
-    print_text_box()
+    print_text_box(scroll)
     stdscr.refresh()
 
 
 def main(stdscr):
+    global quitting
     stdscr.keypad(True)
     stdscr.timeout(TIMEOUT)
 
     key = ''
 
+    # Add initial tab
+    add_new_tab()
+    pause = False
+    scroll = 0
     while key != ord('q'):
-        draw_screen(stdscr)
-
+        draw_screen(stdscr, scroll)
         # Block TIMEOUT ms (100 ms)
         key = stdscr.getch()
 
@@ -189,16 +223,63 @@ def main(stdscr):
             edit_title(stdscr)
         if key == ord('/'):
             set_find(stdscr)
-        if key == ord('q'):
-            break
+        if key == curses.KEY_UP:
+            pristine.pause()
+            scroll -= 1
+        if key == curses.KEY_DOWN:
+            pristine.pause()
+            scroll += 1
+            if scroll >= 0:
+                pristine.unpause()
+                scroll = 0
+        if key == ord('p'):
+            pause = not pause
+            if pause:
+                pristine.pause()
+            else:
+                pristine.unpause()
+
+    quitting = True
+
 
 
 
 def ingest_from_file():
+    global quitting
+
     with open("a.txt") as f:
         for line in f:
-            time.sleep(.2)
+            time.sleep(.05)
             ingest_text(line)
+            if quitting:
+                # The other thread stopped. Say goodbye
+                return
+
+def ingest_from_stdin():
+    global quitting
+    global EOF
+
+    while True:
+        line = sys.stdin.readline()
+
+        if not line:
+            debug()
+            EOF = True
+            quitting = True
+            break
+        ingest_text(line)
+
+    # for line in sys.stdin:
+    #     if not sys.stdin.readable():
+    #         EOF = True
+    #         debug()
+    #     ingest_text(line)
+    #     if quitting:
+    #         # The other thread stopped. Say goodbye
+    #         return
+
+
+
 
 
 if __name__ == "__main__":
@@ -212,9 +293,14 @@ if __name__ == "__main__":
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
 
     x = threading.Thread(target=ingest_from_file)
+    # x = threading.Thread(target=ingest_from_stdin)
     x.start()
 
-    wrapper(main)
+    try:
+        wrapper(main)
+    except Exception:
+        # Let the other thread exist also
+        quitting = True
 
     x.join()
 
