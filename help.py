54b2f683b961bb470c31f2480636daf091dbf0ab
Breno Leitao <leitao@debian.org>
2021-04-07 20:10:33 +0100

    Better Help

--- help.py
+++ help.py
@@ -3,19 +3,21 @@ from curses.textpad import rectangle
 import sys
 import globvar
 
+
 def usage():
     print("Multi tab grepper")
     print("Usage:")
     print(f"  {sys.argv[0]} <file to dump> [-f <profile>]")
 
+
 def show_help(stdscr):
     globvar.pristine.pause()
     mid_x = 4
-    mid_y = int(curses.COLS / 2) - 15
+    mid_y = int(curses.COLS / 2) - 20
 
     stdscr.clear()
     stdscr.refresh()
-    rectangle(stdscr, mid_x, mid_y, mid_x + 10, mid_y + 40)
+    rectangle(stdscr, mid_x, mid_y, mid_x + 16, mid_y + 40)
     stdscr.addstr(mid_x, mid_y + 20, "Help")
     stdscr.addstr(mid_x + 1, mid_y + 2, "a   -   Add a new Tab")
     stdscr.addstr(mid_x + 2, mid_y + 2, "c   -   Toggle case sensitive search")
@@ -25,7 +27,7 @@ def show_help(stdscr):
     stdscr.addstr(mid_x + 6, mid_y + 2, "s   -   Save profile")
     stdscr.addstr(mid_x + 7, mid_y + 2, "l   -   Load profile")
     stdscr.addstr(mid_x + 8, mid_y + 2, "*   -   Highlight an additional word")
-    stdscr.addstr(mid_x + 9, mid_y + 2, "Home, Page UP, Page Down work also")
+    stdscr.addstr(mid_x + 15, mid_y + 2, "Home, Page UP, Page Down work also")
     stdscr.refresh()
     stdscr.timeout(-1)
     stdscr.getch()
